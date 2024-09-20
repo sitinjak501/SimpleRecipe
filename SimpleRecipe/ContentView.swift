721f23b3ef538783f9e62c0de7c67bf0e4f2af2e
Wiranto Millennium S <wirantotinjak@gmail.com>
2024-09-20 19:43:04 +0700

    Initial Commit

---
+++ SimpleRecipe/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SimpleRecipe
+//
+//  Created by wiranto millennium s on 20/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SimpleRecipe/SimpleRecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleRecipeApp.swift
+//  SimpleRecipe
+//
+//  Created by wiranto millennium s on 20/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SimpleRecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
